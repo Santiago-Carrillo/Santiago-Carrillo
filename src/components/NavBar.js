338c2a73b3365f2dbe8d58ef30a787bc2dc53ea7
Santiago Carrilllo <santiaggocarrillo@gmail.com>
2021-06-16 18:48:57 -0300

    primer commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function NavBar(){
+    return (
+        <nav className="navbar navbar-light">
+			<div className="container-fluid">
+                <h1>Libreria el Aleph</h1>
+				<a className="navbar-brand" href="#">1</a>
+			  	<a className="navbar-brand" href="#">2</a>
+			  	<a className="navbar-brand" href="#">3</a>
+			  	<a className="navbar-brand" href="#">4</a>
+			</div>
+		</nav>
+    )
+}
+
+export default NavBar;
