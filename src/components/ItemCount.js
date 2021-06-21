40df9d64a10a29d4f908a0fd1f78a781f217ab0b
Santiago Carrilllo <santiaggocarrillo@gmail.com>
2021-06-21 19:01:52 -0300

    desafio componentes 2, clase05

---
+++ src/components/ItemCount.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+
+const ItemCount = ({stock: initialStock, initial, onAdd}) => {
+    
+    const [count, setCount] = useState(initial)
+    const [stock, setStock] = useState(initialStock)
+
+    const addCount = () => {
+        if(stock >= 1) {
+            setCount(count + 1)
+            setStock(stock - 1)
+            alert("stock: " + stock)
+        }    
+    }
+    const substractCount = () => {
+        setCount(count - 1)
+        setStock(stock + 1)
+        alert("stock: " + stock)
+    }
+
+    return (
+        <div>
+            <button onClick={substractCount}>-</button>
+            {count}
+            <button onClick={addCount}>+</button>
+            <button onClick={onAdd}>Agregar al carrito</button>
+        </div>
+    )
+}
+
+export default ItemCount;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import ItemCount from './ItemCount';
 
 function ItemListContainer(props) {
-    return <h3>Hola, {props.greeting}</h3>;
-  }
+    return (  
+        <div>
+            <h3>Hola, {props.greeting}</h3>
+            <ItemCount stock={3} initial={1} onAdd={() => console.log('agregado')}/>
+        </div>
+    )  
+}
   
 export default ItemListContainer;
