43d566ef4c9e50906d6b2809d5a73de1733506d8
Santiago Carrilllo <santiaggocarrillo@gmail.com>
2021-06-22 11:33:54 -0300

    desafio componentes 2, clase05 corregido

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -4,18 +4,20 @@ const ItemCount = ({stock: initialStock, initial, onAdd}) => {
     
     const [count, setCount] = useState(initial)
     const [stock, setStock] = useState(initialStock)
-
+    
     const addCount = () => {
         if(stock >= 1) {
             setCount(count + 1)
             setStock(stock - 1)
-            alert("stock: " + stock)
+            // alert("stock: " + stock)
         }    
     }
     const substractCount = () => {
+        if(count > 0) {
         setCount(count - 1)
         setStock(stock + 1)
-        alert("stock: " + stock)
+        // alert("stock: " + stock)
+        }
     }
 
     return (
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -5,7 +5,7 @@ function ItemListContainer(props) {
     return (  
         <div>
             <h3>Hola, {props.greeting}</h3>
-            <ItemCount stock={3} initial={1} onAdd={() => console.log('agregado')}/>
+            <ItemCount stock={3} initial={0} onAdd={() => console.log('agregado')}/>
         </div>
     )  
 }
