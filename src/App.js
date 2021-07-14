529cb4552783c44114b95d9a7fe7196bd6ca4f12
Santiago Carrilllo <santiaggocarrillo@gmail.com>
2021-07-14 20:24:29 -0300

    desafio Clase8

--- src/App.js
+++ src/App.js
@@ -2,26 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar.js';
 import ItemListContainer from './components/ItemListContainer';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <ItemListContainer greeting='Bienvenidos!' />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Libreria el Aleph, donde todos los libros convergen</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+        <ItemListContainer greeting='Bienvenidos!' />
+        <Switch>
+          <Route exact path="/">
+            <ItemListContainer />
+          </Route>
+          
+          
+        </Switch>
+      </Router>
     </div>
   );
 }
